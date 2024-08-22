5eaeb570def8b0d3bbd5756190a6c45d8b627211
umairsafdar768 <umairsafdar768@gmail.com>
2024-08-22 16:08:48 +0500

    12th commit

--- main.py
+++ main.py
@@ -91,12 +91,12 @@ def fibonacci(n):
 #     """
 #     return (celsius * 9/5) + 32
 
-# def count_vowels(string):
-#     """
-#     Count the number of vowels in a string
-#     """
-#     vowels = 'aeiouAEIOU'
-#     return sum(1 for char in string if char in vowels)
+def count_vowels(string):
+    """
+    Count the number of vowels in a string
+    """
+    vowels = 'aeiouAEIOU'
+    return sum(1 for char in string if char in vowels)
 
 def is_palindrome(s):
     """
